906ad58ddd3ebbafd399bd8335891cb82f1c6ffa
hellolucient <hellolucient@gmail.com>
2024-10-18 17:26:09 +0700

    Update fetchNEWS.js

--- api/fetchNEWS.js
+++ api/fetchNEWS.js
@@ -1,30 +1,7 @@
-const https = require('https');
-
 module.exports = (req, res) => {
   const { topic } = req.query;
-  const newsApiKey = process.env.NEWS_API_KEY;
-
-  const options = {
-    hostname: 'newsapi.org',
-    path: `/v2/everything?q=${encodeURIComponent(topic)}&apiKey=${newsApiKey}&language=en&pageSize=5`,
-    method: 'GET'
-  };
-
-  const request = https.request(options, (response) => {
-    let data = '';
-
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      res.status(200).json(JSON.parse(data));
-    });
+  res.status(200).json({
+    message: "API is working",
+    receivedTopic: topic
   });
-
-  request.on('error', (error) => {
-    res.status(500).json({ error: 'Error fetching news' });
-  });
-
-  request.end();
 };
